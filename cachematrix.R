75ca3aee4c9c61fbf96d5670ccc818c448909f6b
GTX2090 <53283823+GTX2090@users.noreply.github.com>
2019-08-10 16:39:17 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## this function creates the cache of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y){
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x	
+	inver <- function(inverse){
+		m <<- inverse
+	}
+	getinver <- function() m
+	list(set = set, get = get,inver = inver, getinver = getinver)
 }
 
 
-## Write a short comment describing this function
+## this function solve the inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getinver()
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data,...)
+	x$inver(m)
+	m
+      ## Return a matrix that is the inverse of 'x'
 }
